9d26c3b98b89f7d7e148beca24b99e7a3ed41375
selimdawa <selimdawa@gmail.com>
2026-06-25 01:21:01 +0300

    Kotlin Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp.processor)
     alias(libs.plugins.daggerHiltAndroid)
@@ -37,11 +36,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-        }
     }
     buildFeatures {
         compose = true
